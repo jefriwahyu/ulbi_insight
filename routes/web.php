198c87f9d01675e3a5424dcc28e9602e2eaa8fef
jefriwahyu <92837751+jefriwahyu@users.noreply.github.com>
2024-11-06 16:38:06 +0700

    menambahkan ui

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,21 @@ Route::get('/', function () {
 });
 
 Route::get('/home', function () {
-    return view('home');
+    return view('index');
+});
+
+Route::get('/author', function () {
+    return view('author');
+});
+
+Route::get('/category', function () {
+    return view('category');
+});
+
+Route::get('/details', function () {
+    return view('details');
+});
+
+Route::get('/search', function () {
+    return view('search');
 });
