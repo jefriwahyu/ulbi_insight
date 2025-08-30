176a5d1f595534a4416c0fc9170d314ed2b9440f
jefriwahyu <memberjefry1@gmail.com>
2025-08-30 11:21:13 +0700

    conf vercel

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,21 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                css: 'resources/css/app.css'
+            }
+        }
+    }
 });
